6eb2a4fa512796655cc223d9475b93f8b8c97851
houdayec <houdayer.corentin@gmail.com>
2016-11-23 23:24:14 +0200

    implementation of fab floating buttons menu

---
+++ app/src/main/java/hantizlabs/jeuduroi/GameActivity.java
@@ -0,0 +1,52 @@
+package hantizlabs.jeuduroi;
+
+import android.app.Activity;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+
+        final View actionB = findViewById(R.id.action_b);
+
+        FloatingActionButton actionC = new FloatingActionButton(getBaseContext());
+        actionC.setTitle("Hide/Show Action above");
+        actionC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                actionB.setVisibility(actionB.getVisibility() == View.GONE ? View.VISIBLE : View.GONE);
+            }
+        });
+
+        final FloatingActionsMenu menuMultipleActions = (FloatingActionsMenu) findViewById(R.id.multiple_actions);
+        menuMultipleActions.addButton(actionC);
+
+
+        ShapeDrawable drawable = new ShapeDrawable(new OvalShape());
+        drawable.getPaint().setColor(getResources().getColor(R.color.white));
+
+        final FloatingActionButton actionA = (FloatingActionButton) findViewById(R.id.action_a);
+        actionA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                actionA.setTitle("Action A clicked");
+            }
+        });
+
+
+
+
+    }
+    }
--- app/src/main/java/hantizlabs/jeuduroi/StartActivity.java
+++ app/src/main/java/hantizlabs/jeuduroi/StartActivity.java
@@ -1,13 +1,32 @@
 package hantizlabs.jeuduroi;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class StartActivity extends AppCompatActivity {
+public class StartActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        Button button= (Button) findViewById(R.id.button);
+        button.setOnClickListener((android.view.View.OnClickListener) this);
+
+    }
+
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        switch (v.getId()) {
+            case R.id.button:
+                Intent intent = new Intent(StartActivity.this, GameActivity.class);
+                startActivity(intent);
+                break;
+        }
+
     }
 }
